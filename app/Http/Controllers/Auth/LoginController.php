4f53728752975462602c667ef28e61a621d4510a
Dimitar Markov <dmarkov23@gmail.com>
2017-03-20 11:50:42 +0100

    Login controller namespace fix

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
 class LoginController extends Controller
 {
 
